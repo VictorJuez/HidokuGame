93df489630272aba14964fab2cbcc13cc95fceb4
Victor <victorjuez.alu@gmail.com>
2018-04-19 14:00:44 +0200

    Tests folder added

---
+++ test/Domini/TableroTest.java
@@ -0,0 +1,15 @@
+package Domini;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableroTest {
+
+    @Test
+    public void print() {
+        int r = 4;
+        Assert.assertEquals(4, r);
+    }
+}
