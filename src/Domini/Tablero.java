6cf17bf0246f8c9d3d3ee33320373761f9a1e20d
Victor <victorjuez.alu@gmail.com>
2018-04-28 11:30:35 +0200

    backtracking updated

--- src/Domini/Tablero.java
+++ src/Domini/Tablero.java
@@ -40,75 +40,95 @@ public class Tablero extends Mapa {
     }
 
     private static void backtrackingResolucio(int x, int y, String[][] A, Vector v){
-        System.out.println("Now iterating over: "+x+", "+y);
-        System.out.println(v+"\n");
-        if(y >= A[0].length) {
-            y=0;
-            if(x+1 >= A.length) processa(A);
-            else ++x;
-        }
         if(v.size() == 0) {
             System.out.println("finiished");
             processa(A);
         }
         else{
+            System.out.println("Now iterating over: "+x+", "+y);
+            System.out.println(v+"\n");
             if(A[x][y].equals("?")){
                 System.out.println("Interrogant al ["+x+"],["+y+"]");
                 for(int i =0; i<v.size(); ++i){
                     int aux = (int) v.get(i);
-                    for(int k=1; k<=4; ++k){
-                        int xx = x;
-                        int yy = y;
-                        switch (k){
-                            case 1: //checking left
-                                xx=x;
-                                yy=y-1;
-                                break;
-                            case 2: //checking up
-                                xx=x-1;
-                                yy=y;
-                                break;
-                            case 3: //checking right
-                                xx=x;
-                                yy=y+1;
-                                break;
-                            case 4: //checking down
-                                xx=x+1;
-                                yy=y;
-                                break;
+                    System.out.println("checking if we can insert value: "+aux);
+                    if(posicioCorrecte(x,y,A, (Integer) v.get(i), v)){
+                        System.out.println("Posicio correcte! "+x+", "+y+"| Posem el "+aux);
+                        //checking left
+                        A[x][y] = String.valueOf(aux);
+                        v.remove(i);
+                        if(y+1 == A[0].length) {
+                            if(x+1 == A.length) backtrackingResolucio(0,0, A,v);
+                            else backtrackingResolucio(x+1,0,A,v);
                         }
-
-                        if(posicioCorrecte(xx,yy,A, (Integer) v.get(i))){
-                            System.out.println("Posicio correcte! "+xx+", "+yy+"| Posem el "+aux);
-                            //checking left
-                            A[x][y] = String.valueOf(aux);
-                            v.remove(i);
-                            backtrackingResolucio(x,++y,A,v);
-                            v.add(i,aux);
-                            A[x][y] = "?";
-                        }
+                        else backtrackingResolucio(x,y+1,A,v);
+                        //v.add(i,aux);
+                        //A[x][y] = "?";
                     }
                 }
+                if(y+1 == A[0].length) {
+                    if(x+1 == A.length) backtrackingResolucio(0,0, A,v);
+                    else backtrackingResolucio(x+1,0,A,v);
+                }
+                else backtrackingResolucio(x,y+1,A,v);
             }
             else {
-                backtrackingResolucio(x, ++y, A, v);
+                if(y+1 == A[0].length) {
+                    if(x+1 == A.length) backtrackingResolucio(0,0, A,v);
+                    else backtrackingResolucio(x+1,0,A,v);
+                }
+                else backtrackingResolucio(x,y+1,A,v);
             }
         }
     }
 
-    private static boolean posicioCorrecte(int x, int y, String[][] A, int toInsert){
-        if(x<0 || y<0 || x>=A.length || y>=A[0].length) return false;
-        try {
-            int tableValue = Integer.parseInt(A[x][y]);
-            if (tableValue == toInsert-1 || tableValue == toInsert+1) {
-                System.out.println("tableValue: "+ tableValue + ", toInsert"+ toInsert);
-                return true;
+    private static boolean posicioCorrecte(int x, int y, String[][] A, int toInsert, Vector<Integer> v){
+        boolean adjacentPetit = false;
+        boolean adjacentGran = false;
+        boolean adjacentInterrogant = false;
+        for(int i=0; i<4; ++i){
+            int xx = x;
+            int yy = y;
+            switch (i){
+                case 0:
+                    xx=x;
+                    yy=y-1;
+                    break;
+                case 1:
+                    xx=x-1;
+                    yy=y;
+                    break;
+                case 2:
+                    xx=x;
+                    yy=y+1;
+                    break;
+                case 3:
+                    xx=x+1;
+                    yy=y;
+                    break;
+            }
+            if(xx>=0 && yy>=0 && xx<A.length && yy<A[0].length) {
+
+                System.out.println("Checking position [" + xx + "," + yy + "]");
+
+                if (isInteger(A[xx][yy])) {
+                    System.out.println(A[xx][yy] + " is integer!!");
+                    int tableValue = Integer.parseInt(A[xx][yy]);
+
+                    if (tableValue == toInsert - 1) adjacentPetit = true;
+                    if (tableValue == toInsert + 1) adjacentGran = true;
+                } else if (A[xx][yy].equals("?")) adjacentInterrogant = true;
             }
         }
-        catch (Exception e){
-            return false;
-        }
-        return false;
+
+        if(adjacentGran && adjacentPetit) return true;
+        if(adjacentGran && adjacentInterrogant) if(v.contains(toInsert-1))return true;
+        if(adjacentPetit && adjacentInterrogant) if(v.contains(toInsert+1)) return true;
+
+        if(adjacentPetit && v.size() == 1) return true;
+        if(adjacentGran && v.size() == 1) return true;
+
+    return false;
     }
 
     private static void processa(String[][] A){
@@ -137,6 +157,19 @@ public class Tablero extends Mapa {
         }
         return total;
 
+    }
+
+    public static boolean isInteger(String s) {
+        try
+        {
+            Integer.parseInt(s);
+            // s is a valid integer
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
     }
 
 }
