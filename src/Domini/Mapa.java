61a43a0d6ac4ef150b04d9592489cac304298aa9
Mathiix <mathiasbertorelli@gmail.com>
2018-05-01 17:20:56 +0200

    jajajajaja ekisde

--- src/Domini/Mapa.java
+++ src/Domini/Mapa.java
@@ -208,15 +208,7 @@ public class Mapa {
         Integer[][] casillas_visitadas;
         boolean atrapado = false;
         casillas_visitadas = new Integer[numero_fil][numero_col];
-        for (int i = 0; i < numero_fil; ++i) //en el caso de los hexágonos he de preparar la matriz con las marcas.
-        {
-            for (int j = 0; j < numero_col; ++j)
-            {
-                if (i == 0 && j % 2 == 0) casillas_visitadas[i][j] = -2;
-                else if (i == numero_fil -1 && j % 2 != 0) casillas_visitadas[i][j] = -2;
-                else casillas_visitadas[i][j] = -1;
-            }
-        }
+        for (int i = 0; i < numero_fil; ++i) for (int j = 0; j < numero_col; ++j) casillas_visitadas[i][j] = -1;
         int dir;
         Integer[] ant_casilla = new Integer[2]; //[0] -> filas, [1] -> columnas CASILLA EN LA QUE ESTOY ACTUALMENTE
         Integer[] sig_casilla = new Integer[2]; //[0] -> filas, [1] -> columnas
@@ -225,20 +217,28 @@ public class Mapa {
         ant_casilla[1] = ThreadLocalRandom.current().nextInt(0, numero_col);
 
         casillas_visitadas[ant_casilla[0]][ant_casilla[1]] = 1;
-
+        Integer[] adyacenciashex = new Integer[]{-2, 0, 1, 2, 3, 5};
         //las adyacencias funcionan igual sea costados o costados y angulos.
         boolean normal; //para saber la columna del hexágono (las adyacencias funcionan diferente)
         for (int i = 2; i < casillas_validas + 1 && !atrapado; ++i)
         {
             normal = ant_casilla[1] % 2 == 0; //true -> columna que empieza por -2.
             if (normal) dir = ThreadLocalRandom.current().nextInt(-2, 3 + 1);
-            else dir = ThreadLocalRandom.current().nextInt(0, 5 + 1);
+            else
+            {
+                dir = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+                dir = adyacenciashex[dir];
+            }
             sig_casilla = siguienteCasilla(ant_casilla, dir);
             int intentos = 0;
             while (!casillaValida(sig_casilla[0], sig_casilla[1], numero_fil, numero_col, casillas_visitadas) && !atrapado)
             {
                 if (normal) dir = ThreadLocalRandom.current().nextInt(0, 2 + 1);
-                else dir = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+                else
+                {
+                    dir = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+                    dir = adyacenciashex[dir];
+                }
                 sig_casilla = siguienteCasilla(ant_casilla, dir);
                 intentos += 1;
                 if (intentos == 5) atrapado = true;
