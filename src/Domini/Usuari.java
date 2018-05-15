65f0f9e03df0bae77cef3797a4c76e12fe6fa431
unknown <ebover.centelles@gmail.com>
2018-05-15 16:27:15 +0200

    classe usuari

---
+++ src/Domini/Usuari.java
@@ -0,0 +1,45 @@
+package Domini;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Collections
+import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Usuari {
+    protected String ID;
+    protected static Map <String, String> instances = Collections.emptyMap();  //estructura de datos donde guardaremos todos los usuarios a medida que se van creando y borrando
+    protected static String actual = null;    //usuario con la sesion abierta, no se si es pot fer aixo
+    protected String password;  //password del usuario
+    protected Vector<String> partidas = new Vector<String>();   //partidas del usuario
+    protected Vector<String> mapas = new Vector<String>();      //mapas que ha creado el usuario
+
+    public Usuari(){}   //creadora
+
+    public boolean crearUsuari(String ID, String password){     //retorna true si s'ha creat l'usari amb exit, en cas contrari retorna false
+        if (instances.containsKey(ID)) return false;
+        else{
+            instances.put(ID,password);
+            this.ID = ID;
+            this.password = password;
+        }
+        return true;
+    }
+
+    public void borrarUsuari(){
+        if (actual == this.ID) actual = null;   //si borrem l'usuari actual, aleshores el posem a null
+        instances.remove(ID);
+        this.partidas = new Vector<String>();
+        this.mapas = new Vector<String>();      //reinicialitzem les estructures
+        this.ID = null;
+        this.password = null;
+    }
+
+    public String getID(){
+        return this.ID;
+    }
+
+
+}
