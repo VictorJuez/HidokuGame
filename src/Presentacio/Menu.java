ce8fe1f172df8d2bde8138b5c31d90b8f4370a74
Mathias23 <mathiasbertorelli@gmail.com>
2018-06-01 01:28:29 +0200

    Creador mapa y menu

---
+++ src/Presentacio/Menu.java
@@ -0,0 +1,15 @@
+package Presentacio;
+
+import javax.swing.*;
+
+public class Menu {
+    private JPanel MenuPanel;
+    private JButton RankingButton;
+    private JButton CreadorMapasButton;
+    private JButton JugarButton;
+
+    public Menu ()
+    {
+
+    }
+}
